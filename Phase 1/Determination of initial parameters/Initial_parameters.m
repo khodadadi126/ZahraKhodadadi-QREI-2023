77a0b24a511d1f8fdcc6305911280c4000d5c95b
Zahra Khodadadi <133666701+khodadadi126@users.noreply.github.com>
2023-12-23 13:26:01 +0330

    Update Initial_parameters.m

--- Phase 1/Determination of initial parameters/Initial_parameters.m
+++ Phase 1/Determination of initial parameters/Initial_parameters.m
@@ -3,51 +3,39 @@
 function []=Initial_parameters()   
 
 
-%=============================================================
+
 clc
 clear all
 warning off all
 fclose all;
+pixs=[250;250]; 
+number_of_images=10; 
+init_size=22; 
+grids=[10;10]; 
+increment_size=4;   
+m=10; 
+spaces=pixs./grids;   
 
-%\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Parameters////////////////////////////////%
-pixs=[250;250]; %Number of Pixels to Reshape Image
-number_of_images=10; %Number of Good Images
-init_size=22; %Initial size (width and height) of serveillence region
-grids=[10;10]; %Number of Section to scan...x and y directions
-increment_size=4;   %Increment size of serveillence region....increase in width and height
-m=10; %History Window
-spaces=pixs./grids; %Calculates Max Surviellance Size   
-
-%=====================================
-%===================================
-%Contourlet Transform orders
 Nom=imread('Tile_Nom.bmp');
 Nom=imresize(Nom,[512,512]);
 pfilter='pkva';
 dfilter='pkva';
 nlevels=[1 2];
 y = pdfbdec(double(Nom),pfilter, dfilter, nlevels);
-%figure & ContourletCoeff
+
 ContourletCoeff=showpdfb(y);
 Nom=ContourletCoeff.New;
 Nom=imresize(Nom,[250,250]);
-
-%---------------------------------------
-
    for pic=1:number_of_images 
 
-       %=================================================
         I=imnoise(uint8(Nom),'Poisson');
         I=double(Nom)-double(I); 
-         %=================================================
-    
-
-        %---------------------------------
+         
         for i = 1:grids(1) %Run through x locations for center(i,j} of serveillance box
             for j = 1:grids(2) %Run through y locations for center(i,j} of serveillance box
                 if pic ==1;
                     center(i,j,:)=[spaces(1)/2+(i-1)*spaces(1);spaces(2)/2+(j-1)*spaces(2)]; %Current center(i,j} of serveillance box
-                    min_dis(i,j)=min([squeeze(center(i,j,:))-init_size/2-1;pixs-squeeze(center(i,j,:))-init_size/2-1]); %Find Nearest Edge...Maximum amount the box can increase
+                    min_dis(i,j)=min([squeeze(center(i,j,:))-init_size/2-1;pixs-squeeze(center(i,j,:))-init_size/2-1]); 
                     tempsteps=[init_size:increment_size:init_size+min_dis(i,j)*2]; % Size Increments of the box
                     max_steps(i,j)=length(tempsteps);
                     steps{i,j}=tempsteps;
@@ -59,11 +47,11 @@ Nom=imresize(Nom,[250,250]);
                         end
                         I_2_test=I(I_2_test1_reg{i,j,stepper}(1):I_2_test1_reg{i,j,stepper}(2),I_2_test1_reg{i,j,stepper}(3):I_2_test1_reg{i,j,stepper}(4)); %Take Pixels out of Picture in box
                         number_of_pixs=numel(I_2_test);
-                        I_2_test_reshape=reshape(I_2_test,1,number_of_pixs); %Reshape Pixels into a vector
-                        X_sum(i,j,stepper,pic)=sum(I_2_test_reshape); %Collect sums of diffs
-                        X2_sum(i,j,stepper,pic)=sum(I_2_test_reshape.^2); %Collect sums of diffs
+                        I_2_test_reshape=reshape(I_2_test,1,number_of_pixs);
+                        X_sum(i,j,stepper,pic)=sum(I_2_test_reshape); 
+                        X2_sum(i,j,stepper,pic)=sum(I_2_test_reshape.^2); 
                         if pic==1
-                            X_count(i,j,stepper)=number_of_pixs; %Collect nums of diffs
+                            X_count(i,j,stepper)=number_of_pixs; 
                             X_count2(i,j,stepper)=number_of_pixs;
                         end
                     else
@@ -77,13 +65,13 @@ Nom=imresize(Nom,[250,250]);
                         I_2_test2=I(I_2_test2_reg{i,j,stepper}(1):I_2_test2_reg{i,j,stepper}(2),I_2_test2_reg{i,j,stepper}(3):I_2_test2_reg{i,j,stepper}(4)); %Pixels on Bottom of box
                         I_2_test3=I(I_2_test3_reg{i,j,stepper}(1):I_2_test3_reg{i,j,stepper}(2),I_2_test3_reg{i,j,stepper}(3):I_2_test3_reg{i,j,stepper}(4));  %Pixels on Left of box
                         I_2_test4=I(I_2_test4_reg{i,j,stepper}(1):I_2_test4_reg{i,j,stepper}(2),I_2_test4_reg{i,j,stepper}(3):I_2_test4_reg{i,j,stepper}(4));  %Pixels on Right of box
-                        I_2_test=[I_2_test1 I_2_test2 I_2_test3' I_2_test4']; %Take Pixels on edge of box
+                        I_2_test=[I_2_test1 I_2_test2 I_2_test3' I_2_test4']; 
                         number_of_pixs=numel(I_2_test);
-                        I_2_test_reshape=reshape(I_2_test,1,number_of_pixs); %Reshape Pixels into a vector
-                        X_sum(i,j,stepper,pic)=sum(I_2_test_reshape)+X_sum(i,j,stepper-1,pic); %Collect sums of diffs
-                        X2_sum(i,j,stepper,pic)=sum(I_2_test_reshape.^2)+X2_sum(i,j,stepper-1,pic); %Collect sums of diffs
+                        I_2_test_reshape=reshape(I_2_test,1,number_of_pixs); 
+                        X_sum(i,j,stepper,pic)=sum(I_2_test_reshape)+X_sum(i,j,stepper-1,pic); 
+                        X2_sum(i,j,stepper,pic)=sum(I_2_test_reshape.^2)+X2_sum(i,j,stepper-1,pic); 
                         if pic==1
-                            X_count(i,j,stepper)=number_of_pixs; %Collect nums of diffs
+                            X_count(i,j,stepper)=number_of_pixs; 
                             X_count2(i,j,stepper)=number_of_pixs+X_count2(i,j,stepper-1);
                         end
                     end
@@ -113,17 +101,17 @@ for pic = 1:number_of_images
     for i = 1:grids(1) %Run through x locations for center of serveillance box
         for j = 1:grids(2) %Run through y locations for center of serveillance box
             for stepper=1:max_steps(i,j)
-                mu_est=X_sum(i,j,stepper,pic-mflag)/X_count2(i,j,stepper); %Sample mean from box
+                mu_est=X_sum(i,j,stepper,pic-mflag)/X_count2(i,j,stepper); 
                 rat=lratconst(i,j,stepper)*X_count2(i,j,stepper)*(mu_est-mus(i,j,stepper))^2;
                 rat_hist(i,j,stepper,pic)=rat;
-                if rat>ratio %If the statistic is bigger than previous one
-                    ratio=rat; %New statistic
+                if rat>ratio 
+                    ratio=rat;
                 end
                 for pic_retro=pic-1:-1:1+mflag
                     %pic_retro
                     X_counttemp=(pic-pic_retro+1)*X_count2(i,j,stepper);
                     X_sum(i,j,stepper,pic_retro-mflag)=X_sum(i,j,stepper,pic_retro-mflag)+X_sum(i,j,stepper,pic-mflag);
-                    mu_est=X_sum(i,j,stepper,pic_retro-mflag)/X_counttemp; %Sample mean from box
+                    mu_est=X_sum(i,j,stepper,pic_retro-mflag)/X_counttemp; 
                     rat=lratconst(i,j,stepper)*X_counttemp*(mu_est-mus(i,j,stepper))^2;
                     if pic>=11
                       %  rat
